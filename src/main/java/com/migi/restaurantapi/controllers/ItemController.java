fee42cf1ec7d39aa7c58c737c76e3df9a05a204e
Mrigendra Kr <mrigendra.kumar@lenskart.in>
2021-07-06 23:22:44 +0530

    CORS enabled on controller level

--- src/main/java/com/migi/restaurantapi/controllers/ItemController.java
+++ src/main/java/com/migi/restaurantapi/controllers/ItemController.java
@@ -11,6 +11,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RequestMapping("/api/v1/items")
 public class ItemController {
 
